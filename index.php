7d1a2e5f0fd03fda74ca8b4bc7864a031f9983df
Nayan <nayan88@users.noreply.github.com>
2014-07-28 17:26:31 +0530

    Create index.php
    Index file

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>    
+    <title> Reef Lovers Aquarium </title>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+    
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    
+    <!--css styles starts-->
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/royalslider.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" media="screen" >
+	<link href="css/royalslider.css" rel="stylesheet">
+    <!--css styles ends-->
+     
+    <!--google/webkitt fonts embeddeds here-->
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
+    <!--google/webkitt fonts embeddeds here-->
+    
+    <!--script  starts-->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="css/ie.css">
+    <![endif]-->
+     <!--script  ends-->
+     
+    <!--common jquery starts-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/jquery.royalslider.min.js"></script>
+ 	<script src="js/jquery.slicknav.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#menu').slicknav();			
+	    $('#full-width-slider').royalSlider({
+		arrowsNav: true,
+		loop: false,
+		keyboardNavEnabled: true,
+		controlsInside: false,
+		imageScaleMode: 'fill',
+		arrowsNavAutoHide: false,
+		autoScaleSlider: true,
+		controlNavigation: 'bullets',
+		thumbsFitInViewport: false,
+		navigateByClick: true,
+		startSlideId: 0,		
+		transitionType:'move',
+		globalCaption: false,
+		transitionSpeed: 600,
+		deeplinking: {
+		  enabled: true,
+		  change: false
+		},				
+		autoScaleSlider: true,
+    	autoPlay: {
+    		// autoplay options go gere
+    		enabled: true,
+    		pauseOnHover: true
+    	}
+		});			
+		});		
+	</script>
+    <script src="js/css_browser_selector.js" type="text/javascript"></script>
+     <!--common jquery end-->
+</head>
+
+<body>
+	<div class="wrapper">
+    	<!--header section starts-->
+        <?php include('header.php'); ?>
+        <!--header section ends-->           
+        <!-- midd section starts here-->
+        <section>
+        	<div id="mid-container">
+            	<div class="home-banner">
+                    	<div class="main">
+                    		<div class="banner-maintitle">
+                        		<p>Bringing the Ocean to<br> Your Home or Office</p>
+                        	</div>    
+                    		<div id="full-width-slider" class="royalSlider heroSlider rsMinW">
+  								<div class="rsContent">
+									<img class="rsImg" src="images/banner1.jpg" alt="banner1">
+    								<div class="infoBlock" data-fade-effect="" data-move-offset="10" data-move-effect="bottom">
+                                    	<h2>Residential Custom Aquariums</h2>
+                                        <div class="banner-des">
+                                        	We can design an aquarium to accent any decor, and fit in just about any space. Combining unique aquarium <br>
+										designs with tailored aquarium furniture, we can bring beautifuly living art to your home. 
+                                        <a class="blearn-btn" href="#" title="Learn More">Learn More » </a>                                         	                                          	
+										</div>
+                                    </div>
+  								</div>
+                                <div class="rsContent">
+									<img class="rsImg" src="images/banner2.jpg" alt="banner2">
+    								<div class="infoBlock" data-fade-effect="" data-move-offset="10" data-move-effect="bottom">
+                                    	<h2>Commercial Aquariums</h2>
+                                        <div class="banner-des">
+                                        	We specialize in all types of commercial holding systems - from simple 100 gallon lobster holding tanks to several hundred<br> gallon sophisticated crab and crawfish holding systems. There is no system too big or too small. We can design, install and <br> maintain a holding system tailored to your business needs and budget. 
+										</div>
+                                    </div>
+  								</div>
+                                <div class="rsContent">
+									<img class="rsImg" src="images/banner3.jpg" alt="banner3">
+    								<div class="infoBlock" data-fade-effect="" data-move-offset="10" data-move-effect="bottom">
+                                    	<h2>Visit our retail store in Wellington</h2>
+                                        <div class="banner-des">
+                                        	We have a wide variety of corals, fish and supplies to choose from with new shipments coming in every week.
+										</div>
+                                    </div>
+  								</div>
+  							</div>
+                            <!-- For device banner section starts here-->
+                            <div class="device-banner">
+                            	<div class="device-banner-con">
+                            	<p>Visit our retail store<br>in Wellington</p>
+                            </div>    
+                            	<div class="device-banner-des">
+                            		<p>We have a wide variety of corals, fish<br> and supplies to choose from with new<br> shipments coming in every week.</p>
+                            	</div>    
+                            </div>
+                            <!-- For device banner section end here-->
+						</div>
+						<div class="clear"></div>
+                    </div>
+                <div class="allreef-des">  
+                    	<div class="main">
+	                		<!-- mid section codes here-->
+                        	<div class="left-panel">
+                        		<section>
+                                	<h2 class="rf-title">Reef Lovers provides professional aquarium services and custom aquarium installations</h2>
+                                	<div class="rf-des">
+                                	<p>We work with the latest technologies and pay meticulous attention to detail. Our educated and experienced staff offers complete aquarium care. Our #1 goal is excellence   											                                    and complete customer satisfaction. Reef Lovers is committed to providing elegant, high quality and sophisticated aquariums at competitive prices.</p>
+                                </div>   
+                               </section>                                                        
+                        	</div>
+              		    	<aside>
+                            	<h2 class="rf-title">Custom Aquarium of the Month</h2>
+                            	<div class="rf-des">                            	
+                            		<div class="rf-pic"><img class="fl" src="images/aqu-month.jpg" alt="Custom Aquarium of the Month" title="Custom Aquarium of the Month"></div>
+                                	<p>450 gallon fully automated custom live reef aquarium for a home in Boca Raton, FL.</p>
+                            	</div>    
+                        	</aside>                		
+                        	<div class="clear"></div>
+                        	<!-- mid section codes ends here-->
+                    	</div>	                      
+                    </div>
+             </div>
+        </section>
+        <!-- midd section ends here-->
+        <!-- initialize push div for sticky footer starts here-->
+        <div class="push"></div>
+        <!-- initialize push div for sticky footer ends here-->
+    </div>
+    <!--footer starts here-->
+    <?php include('footer.php'); ?>
+     <!-- image preloads starts here-->
+   	 <div class="preloader"><img src="images/logo.png" alt="Reef Lover"> <img src="images/fb1.png" alt="Social"></div>
+     <!-- image preloads ends here-->
+    <!--footer ends here-->
+    <!--scripts starts here--> 
+    <script src	="js/modernizr-2.7.1.min.js"></script>
+    <!--scripts ends here--> 
+</body>
+</html>
